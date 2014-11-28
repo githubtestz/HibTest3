352ea44be31e801c2d4f8f0edef3170eef1218fc
githubtestz <gedszef@gmail.com>
2014-11-29 00:55:29 +0100

    hello world

--- src/UserDetails.java
+++ src/UserDetails.java
@@ -3,4 +3,26 @@
  */
 public class UserDetails
 {
+    private int userId;
+    private String username;
+
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
 }
