43bf99ab04d240494874f586d4c1dd18e7281138
githubtestz <gedszef@gmail.com>
2014-12-01 23:59:27 +0100

    camelCase for userName fixed

--- src/UserDetails.java
+++ src/UserDetails.java
@@ -4,25 +4,25 @@
 public class UserDetails
 {
     private int userId;
-    private String username;
-
-    public void setUserId(int userId)
-    {
-        this.userId = userId;
-    }
-
-    public void setUsername(String username)
-    {
-        this.username = username;
-    }
+    private String userName;
 
     public int getUserId()
     {
         return userId;
     }
 
-    public String getUsername()
+    public void setUserId(int userId)
     {
-        return username;
+        this.userId = userId;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
     }
 }
